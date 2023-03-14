3aeb864e6294670e5c13c4a32256cac7e9aaa1a8
malcolmPark <122580137+malcolmPark@users.noreply.github.com>
2023-03-13 17:39:12 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,15 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void averageWithoutLowest() {
+    double[] input1 = { 1.0, 1.0, 2.0, 3.0 };
+    assertEquals(ArrayExamples.averageWithoutLowest(input1), 2.0, 0);
   }
 }
